ff295aee6c869192e4cc8bbc28c486422ae9e7d5
yRodrigo2219 <rodrigo2219dg@gmail.com>
2022-06-13 22:56:09 -0300

    Página de Login

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuth } from "./contexts/auth";
 import Login from "./pages/Auth/Login";
+import Profile from "./pages/App/Profile";
 
 export default function AppRoutes() {
+  const { signed } = useAuth();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={signed ? <Profile own /> : <Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,3 +1,62 @@
+import {
+  Input,
+  Center,
+  Stack,
+  Paper,
+  Image,
+  Text,
+  PasswordInput,
+  Button,
+  Anchor,
+  Checkbox,
+} from "@mantine/core";
+import { Link } from "react-router-dom";
+import { Mail, Lock } from "tabler-icons-react";
+
+import FullLogo from "../../assets/FullLogo.png";
+
 export default function LoginPage() {
-  return <div>Ola login!</div>;
+  return (
+    <Center style={{ minHeight: "100vh" }}>
+      <Paper
+        shadow="sm"
+        p="xl"
+        withBorder
+        style={{ maxWidth: "90vw", width: "25rem" }}
+        sx={(theme) => ({
+          [theme.fn.smallerThan("xs")]: {
+            border: "none",
+            boxShadow: "none",
+            padding: "0.5rem",
+          },
+        })}
+      >
+        <Stack justify="flex-start" spacing="xl">
+          <Center>
+            <Image src={FullLogo} alt="Fazeres" style={{ maxWidth: "18rem" }} />
+          </Center>
+          <Text align="center" size="lg">
+            Bem vindo de volta ao aplicativo do <b>Fazeres</b>
+          </Text>
+
+          <Stack justify="flex-start" spacing="xs">
+            <Input size="md" icon={<Mail />} placeholder="Email" />
+            <Stack justify="flex-start" spacing="xs">
+              <PasswordInput size="md" icon={<Lock />} placeholder="Senha" />
+              <Checkbox label="Lembrar de mim" />
+            </Stack>
+          </Stack>
+
+          <Button size="md">Entrar</Button>
+
+          <Text align="center">
+            Não tem conta ainda?{" "}
+            <Anchor component={Link} to="/registro">
+              Criar conta agora
+            </Anchor>
+          </Text>
+        </Stack>
+      </Paper>
+    </Center>
+  );
 }
